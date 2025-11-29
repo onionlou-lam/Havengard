5f413f9089288d3fa0b94603e7958f942cd8f358
Liam <58209649+Veggipuppet1@users.noreply.github.com>
2025-11-29 17:46:53 +0800

    start building tower logic

---
+++ Assets/Scripts/Units/Towers/FrostTower.cs
@@ -0,0 +1,22 @@
+using Havengard.Units;
+using UnityEngine;
+
+public class FrostTower : UnitBase
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    override protected GameObject FindTarget()
+    {
+        //
+    }
+}
