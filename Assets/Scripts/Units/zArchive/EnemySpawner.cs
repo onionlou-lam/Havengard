c0bec95142fdd66b302912f269a9d71cfbaea1a2
Lucas O <lucas.olynyk@gmail.com>
2026-01-20 10:45:53 +0800

    Health flashes + Ability bar done

--- Assets/Scripts/Units/zArchive/EnemySpawner.cs
+++ Assets/Scripts/Units/zArchive/EnemySpawner.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+﻿/*using UnityEngine;
 using Havengard.Units;   // ⬅️ important
 
 namespace Havengard.Units
@@ -23,3 +23,4 @@ namespace Havengard.Units
         }
     }
 }
+*/
