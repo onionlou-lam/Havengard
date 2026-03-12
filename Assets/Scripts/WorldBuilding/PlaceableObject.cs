3d0fed9db7d8834e4a8e91e28cf10597d94ddbf3
Liam <liam.goeng@gmail.com>
2026-03-12 11:12:11 +0800

    Revert to .32 cell sizes to conform to main game

--- Assets/Scripts/WorldBuilding/PlaceableObject.cs
+++ Assets/Scripts/WorldBuilding/PlaceableObject.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 public class PlaceableObject : MonoBehaviour
 {
     [SerializeField] private Vector3 boxOffset = Vector3.zero;
-    [SerializeField] private Vector3 boxSize = new Vector3(1f, 1f, 0f);
+    [SerializeField] private Vector3 inputBoxSize = new Vector3(1f, 1f, 0f);
     [SerializeField] private Color gizmoColor = Color.green;
+    [Tooltip("Set to the grid cell size")] public float cellSize = .32f;
+    [SerializeField, Tooltip("Debug only field")] private Vector3 boxSize;
 
     public Vector3 getBuildingCenter()
     {
@@ -13,10 +15,13 @@ public class PlaceableObject : MonoBehaviour
 
     void OnValidate()
     {
+        inputBoxSize.x = Mathf.Round(inputBoxSize.x);
+        inputBoxSize.y = Mathf.Round(inputBoxSize.y);
+        inputBoxSize.z = Mathf.Round(inputBoxSize.z);
         boxSize = new Vector3(
-            Mathf.Round(boxSize.x),
-            Mathf.Round(boxSize.y),
-            Mathf.Round(boxSize.z)
+            cellSize * Mathf.Round(inputBoxSize.x),
+            cellSize * Mathf.Round(inputBoxSize.y),
+            cellSize * Mathf.Round(inputBoxSize.z)
         );
     }
 
